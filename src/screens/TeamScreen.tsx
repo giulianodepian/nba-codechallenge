f8175ca756f134c62b1a8703f7ff89bbe84af2ea
giuliano <giuliano@pop-os.localdomain>
2022-06-09 15:39:01 -0300

    Added search bar on Team Screen

--- src/screens/TeamScreen.tsx
+++ src/screens/TeamScreen.tsx
@@ -1,18 +1,33 @@
 import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {RootStackScreenProps} from '../types/navigation';
 import useGetPlayersByTeam from '../hooks/useGetPlayersByTeam';
+import SearchBar from "react-native-dynamic-search-bar";
 
 const TeamScreen = ({route}: RootStackScreenProps<'TeamScreen'>) => {
 
     const navigation = useNavigation();
     const {team} = route.params;
+    const [searchText, setSearchText] = useState<string>('');
 
     const {data, loading, error} = useGetPlayersByTeam(team.Key);
 
+    const handleOnChangeText = (text: string) => {
+        setSearchText(text);
+    }
+
+    const handleOnClearText = () => {
+        setSearchText('');
+    }
+
     return (
         <View style={styles.center}>
+            <SearchBar
+                onChangeText={handleOnChangeText}
+                onClearPress={handleOnClearText}
+            />
             <FlatList 
                 data={data}
                 renderItem={({item}) => (
@@ -22,10 +37,12 @@ const TeamScreen = ({route}: RootStackScreenProps<'TeamScreen'>) => {
                                 player: item
                             }
                         )
-                    }}>
+                    }}> 
+                        { (item.FirstName.includes(searchText) || item.LastName.includes(searchText)) &&
                         <View style={styles.playernamebox}>
                             <Text style={styles.playernametext}>{item.FirstName} {item.LastName}</Text>
                         </View>
+                        }
                     </TouchableOpacity>
                 )}
             />
